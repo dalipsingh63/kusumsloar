fc8e4228b98bdb134993eca68307be69f6563e72
dalipsingh63 <dalipsingh2863@gmail.com>
2026-01-27 13:20:01 +0530

    Update api.js

--- light-house/src/services/api.js
+++ light-house/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "/api",
+  withCredentials: true,
 });
 
 export default API;
@@ -10,8 +11,7 @@ export default API;
 // import axios from "axios";
 
 // const API = axios.create({
-//   baseURL: "/api",
-//   withCredentials: true,
+//   baseURL: "http://localhost:5000/api",
 // });
 
 // export default API;

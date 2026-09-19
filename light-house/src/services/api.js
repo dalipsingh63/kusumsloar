ea454c8b0d72ef6db3c3f9e5731a5d400ec30232
dalipsingh63 <dalipsingh2863@gmail.com>
2026-09-19 10:17:27 +0530

    Update api.js

--- light-house/src/services/api.js
+++ light-house/src/services/api.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API = axios.create({
-  baseURL: "/api",
-  withCredentials: true,
-});
+// const API = axios.create({
+//   baseURL: "/api",
+//   withCredentials: true,
+// });
 
-export default API;
+// export default API;
 
 
 // import axios from "axios";
@@ -15,3 +15,17 @@ export default API;
 // });
 
 // export default API;
+
+
+
+// ye api jab fontend vercel pr ho backend render pr to tb 
+
+
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "https://kusumsloar-3backend.onrender.com/api",
+  withCredentials: true,
+});
+
+export default API;

bb1f82862dc089239d86cc7c69c83d5ff916f542
dalipsingh63 <dalipsingh2863@gmail.com>
2026-09-19 12:02:27 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -58,6 +58,74 @@
 
 
 
+// import express from "express";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import connectDB from "./config/db.js";
+// import path from "path";
+// import { fileURLToPath } from "url";
+
+// // Routes
+// import userSignupRoute from "./routes/userSignupRoute.js";
+// import bannerIRoutes from "./routes/bannerIRoutes.js";
+// import cardsRoute from "./routes/cards.js";
+// import bookingRoutes from "./routes/bookingRoutes.js";
+// import videoRoutes from "./routes/videoRoutes.js";
+// import visitorRoutes from "./routes/visitorRoutes.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// // ===== MIDDLEWARE =====
+// app.use(
+//   cors({
+//     origin: [
+//       "https://kusumsloar.vercel.app",
+//       "https://kusumsolar.com",
+//     ],
+//     credentials: true,
+//   })
+// );
+
+// app.use(express.json());
+
+// // ===== DB =====
+// connectDB();
+
+// // ===== PATH FIX =====
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+
+// // ===== API ROUTES =====
+// app.use("/api/user", userSignupRoute);
+// app.use("/api/banner-images", bannerIRoutes);
+// app.use("/api/cards", cardsRoute);
+// app.use("/api/bookings", bookingRoutes);
+// app.use("/api/videos", videoRoutes);
+// app.use("/api/visitors", visitorRoutes);
+
+// // ===== FRONTEND BUILD =====
+// const frontendPath = path.join(__dirname, "../light-house/dist");
+// app.use(express.static(frontendPath));
+
+// // React Router fallback (API excluded)
+// app.get(/^\/(?!api).*/, (req, res) => {
+//   res.sendFile(path.join(frontendPath, "index.html"));
+// });
+
+// // ===== SERVER =====
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+
+
+
+
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -83,6 +151,7 @@ app.use(
     origin: [
       "https://kusumsloar.vercel.app",
       "https://kusumsolar.com",
+      "https://www.kusumsolar.com",
     ],
     credentials: true,
   })
@@ -96,7 +165,6 @@ connectDB();
 // ===== PATH FIX =====
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 // ===== API ROUTES =====
 app.use("/api/user", userSignupRoute);
@@ -117,6 +185,7 @@ app.get(/^\/(?!api).*/, (req, res) => {
 
 // ===== SERVER =====
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
